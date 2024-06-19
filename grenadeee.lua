9c02f80ae5efab07a04e822fc3ded225786d6d90
LazyNoob <105753113+LazyNoobGithub@users.noreply.github.com>
2024-06-18 22:20:40 -0400

    lethal2

--- grenadeee.lua
+++ grenadeee.lua
@@ -20,7 +20,11 @@ Tool0 = Instance.new("Tool")
 Part1 = Instance.new("Part")
 SpecialMesh2 = Instance.new("SpecialMesh")
 Script3 = Instance.new("Script")
-Tool0.Name = "Grenade"
+Tool4 = Instance.new("Tool")
+Part5 = Instance.new("Part")
+SpecialMesh6 = Instance.new("SpecialMesh")
+Script7 = Instance.new("Script")
+Tool0.Name = "Lethal Grenade"
 Tool0.Parent = mas
 Part1.Name = "Handle"
 Part1.Parent = Tool0
@@ -42,9 +46,53 @@ SpecialMesh2.MeshId = "rbxassetid://431532852"
 SpecialMesh2.Scale = Vector3.new(0.0028335859533399343, 0.0023949481546878815, 0.0028335859533399343)
 SpecialMesh2.TextureId = "rbxassetid://431532899"
 SpecialMesh2.MeshType = Enum.MeshType.FileMesh
-Script3.Name = "GrenadeServer"
+Script3.Name = "LethalGrenadeServer"
 Script3.Parent = Tool0
 table.insert(cors,sandbox(Script3,function()
+local MainGrenade = script.Parent.Handle
+local Debris = game:GetService("Debris")
+
+script.Parent.Activated:Connect(function()
+	local GrenadeClone = MainGrenade:Clone()
+	GrenadeClone.CanCollide = true
+	GrenadeClone.Position = MainGrenade.Position
+	GrenadeClone.Parent = workspace
+	Debris:AddItem(GrenadeClone,5)
+	wait(4.8)
+	local Explosion = Instance.new("Explosion",GrenadeClone)
+	Explosion.Position = GrenadeClone.Position
+	Explosion.Hit:Connect(function(prt,distance)
+		if not (distance > 100) then
+			prt.Anchored = false
+		end
+	end)
+end)
+end))
+Tool4.Name = "Grenade"
+Tool4.Parent = mas
+Part5.Name = "Handle"
+Part5.Parent = Tool4
+Part5.CFrame = CFrame.new(17.4197578, 0.391387939, -49.4482269, -1, 0, 0, 0, 1, 0, 0, 0, -1)
+Part5.Orientation = Vector3.new(0, 180, 0)
+Part5.Position = Vector3.new(17.419757843017578, 0.391387939453125, -49.44822692871094)
+Part5.Rotation = Vector3.new(180, 0, 180)
+Part5.Color = Color3.new(0.152941, 0.27451, 0.176471)
+Part5.Size = Vector3.new(0.8049530386924744, 0.7827564477920532, 0.8049530386924744)
+Part5.BottomSurface = Enum.SurfaceType.Smooth
+Part5.BrickColor = BrickColor.new("Earth green")
+Part5.LeftSurface = Enum.SurfaceType.SmoothNoOutlines
+Part5.TopSurface = Enum.SurfaceType.Smooth
+Part5.brickColor = BrickColor.new("Earth green")
+Part5.FormFactor = Enum.FormFactor.Custom
+Part5.formFactor = Enum.FormFactor.Custom
+SpecialMesh6.Parent = Part5
+SpecialMesh6.MeshId = "rbxassetid://431532852"
+SpecialMesh6.Scale = Vector3.new(0.0028335859533399343, 0.0023949481546878815, 0.0028335859533399343)
+SpecialMesh6.TextureId = "rbxassetid://431532899"
+SpecialMesh6.MeshType = Enum.MeshType.FileMesh
+Script7.Name = "GrenadeServer"
+Script7.Parent = Tool4
+table.insert(cors,sandbox(Script7,function()
 local MainGrenade = script.Parent.Handle
 local Debris = game:GetService("Debris")
 
@@ -60,7 +108,7 @@ script.Parent.Activated:Connect(function()
 end)
 end))
 for i,v in pairs(mas:GetChildren()) do
-	v.Parent = owner.Backpack
+	v.Parent = workspace
 	pcall(function() v:MakeJoints() end)
 end
 mas:Destroy()
