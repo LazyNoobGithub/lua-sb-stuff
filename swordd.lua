2f21cb9e8154d45df7ebfbf228bc1fdef6e07c25
LazyNoob <105753113+LazyNoobGithub@users.noreply.github.com>
2024-06-20 17:50:29 -0400

    Update and rename sword.lua to swordd.lua

--- swordd.lua
+++ swordd.lua
@@ -14,6 +14,25 @@ function sandbox(var,func)
 	setfenv(func,newenv)
 	return func
 end
+function ragdoll(plr)
+	for index,joint in pairs(plr.Character:GetDescendants()) do
+		if joint:IsA("Motor6D") then
+			local socket = Instance.new("BallSocketConstraint")
+			local a1 = Instance.new("Attachment")
+			local a2 = Instance.new("Attachment")
+			a1.Parent = joint.Part0
+			a2.Parent = joint.Part1
+			socket.Parent = joint.Parent
+			socket.Attachment0 = a1
+			socket.Attachment1 = a2
+			a1.CFrame = joint.C0
+			a2.CFrame = joint.C1
+			socket.LimitsEnabled = true
+			socket.TwistLimitsEnabled = true
+			joint:Destroy()
+		end
+	end
+end
 cors = {}
 mas = Instance.new("Model",game:GetService("Lighting"))
 Tool0 = Instance.new("Tool")
